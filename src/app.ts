52f33c7d045a34d668618e1b7046f9be2d4eefb6
amuileobsda <legend_gil@naver.com>
2023-09-30 22:39:02 +0900

    errorHandling type add

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,14 @@ import cors from 'cors'
 import IndexRoutes from './routes/index.routes'
 import PostRoutes from './routes/post.routes'
 
+// 허용 url
+const local_whitelist: string[] = ["http://localhost:3000", "http://localhost:8000","http://127.0.0.1:3000", "http://127.0.0.1:8000"];
+
+const whitelist: string[] = ["https://next-ts.ebosda.com", "http://next-ts.ebosda.com", "next-ts.ebosda.com"]; 
+
 // CORS 미들웨어를 추가하여 원하는 도메인에서만 요청 허용
 const corsOptions = {
-    origin: 'https://next-ts.ebosda.com', // 원하는 도메인 주소로 변경
+    origin: whitelist, // 원하는 도메인 주소로 변경
     optionsSuccessStatus: 200, // 성공 상태 코드
 };
 
@@ -43,10 +48,9 @@ export class App {
     private errorHandling() {
         // 잘못된 도메인에서의 요청을 처리하는 미들웨어 추가
         this.app.use((req, res, next) => {
-            // console.log(req.headers.origin);
-            // console.log(corsOptions.origin);
-          if (!req.headers.origin || req.headers.origin !== corsOptions.origin) {
-            return res.status(403).json({ message: 'Forbidden: 요청이 허용되지 않았습니다.' });
+          let headers_origin: string | undefined = req.headers.origin as string;
+          if (!headers_origin || !whitelist.includes(headers_origin)) {
+            return res.status(403).json({ message: '요청이 허용되지 않았습니다.' });
           }
           next();
         });
